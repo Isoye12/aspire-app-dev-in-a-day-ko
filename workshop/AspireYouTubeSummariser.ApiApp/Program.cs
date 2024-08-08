e3ced6bc257f4968ac3bcc2f5e86c35f837f8568
Isoye12 <0150371278i@gmail.com>
2024-08-08 21:50:45 +0900

    action

--- workshop/AspireYouTubeSummariser.ApiApp/Program.cs
+++ workshop/AspireYouTubeSummariser.ApiApp/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 builder.Services.AddHttpClient<IYouTubeVideo, YouTubeVideo>();
-builder.Services.AddScoped<AzureOpenAIClient>(sp =>
+builder.Services.AddScoped<OpenAIClient>(sp =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
     var endpoint = new Uri(config["OpenAI:Endpoint"]);
